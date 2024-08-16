d9c8431c2ac4699adc5e158ba4c34982db2e3f17
MARTTELO <marttelo@gmail.com>
2024-08-16 00:19:34 -0300

    modificando layout total carrinho

--- src/app/(loja)/carrinho/page.tsx
+++ src/app/(loja)/carrinho/page.tsx
@@ -1,33 +1,35 @@
-'use client'
+"use client";
 
-import AreaItemCarrinho from '@/components/carrinho/AreaItemCarrihno'
-import CarrinhoVazio from '@/components/carrinho/CarrinhoVazio'
-import TotalCarrinho from '@/components/carrinho/TotalCarrinho'
-import Pagina from '@/components/template/Pagina'
-import useCarrinho from '@/data/hooks/useCarrinho'
+import AreaItemCarrinho from "@/components/carrinho/AreaItemCarrihno";
+import CarrinhoVazio from "@/components/carrinho/CarrinhoVazio";
+import TotalCarrinho from "@/components/carrinho/TotalCarrinho";
+import Pagina from "@/components/template/Pagina";
+import useCarrinho from "@/data/hooks/useCarrinho";
 
 export default function PaginaCarrinho() {
-    const { itens, adicionar, remover, finalizarCompra } = useCarrinho()
+  const { itens, adicionar, remover, finalizarCompra } = useCarrinho();
 
-    return (
-        <Pagina className="flex flex-col gap-10">
-            {itens.length === 0 ? (
-                <CarrinhoVazio />
-            ) : (
-                <>
-                    <div className="flex flex-col gap-5">
-                        {itens.map((item) => (
-                            <AreaItemCarrinho
-                                key={item.produto.id}
-                                item={item}
-                                adicionar={(item) => adicionar(item.produto)}
-                                remover={(item) => remover(item.produto)}
-                            />
-                        ))}
-                    </div>
-                    <TotalCarrinho itens={itens} finalizar={() => finalizarCompra()} />
-                </>
-            )}
-        </Pagina>
-    )
+  return (
+    <Pagina className="flex flex-col gap-10">
+      {itens.length === 0 ? (
+        <CarrinhoVazio />
+      ) : (
+        <>
+          <div className="flex flex-col gap-5 mt-6 p-8">
+            {itens.map((item) => (
+              <AreaItemCarrinho
+                key={item.produto.id}
+                item={item}
+                adicionar={(item) => adicionar(item.produto)}
+                remover={(item) => remover(item.produto)}
+              />
+            ))}
+          </div>
+          <div className="mt-5 p-8">
+            <TotalCarrinho itens={itens} finalizar={() => finalizarCompra()} />
+          </div>
+        </>
+      )}
+    </Pagina>
+  );
 }
--- src/components/carrinho/TotalCarrinho.tsx
+++ src/components/carrinho/TotalCarrinho.tsx
@@ -10,7 +10,7 @@ export default function TotalCarrinho(props: TotalCarrinhoProps) {
     const total = props.itens.reduce((acc, item) => acc + item.produto.preco * item.quantidade, 0)
 
     return (
-        <div className="flex items-center justify-between bg-zinc-900 rounded-md p-7">
+        <div className="flex items-center justify-between bg-zinc-800 rounded-md p-7">
             <div className="flex flex-col justify-between">
                 <span className=" text-zinc-500">Total</span>
                 <span className="text-3xl font-bold text-yellow-500">R$ {total.toFixed(2)}</span>
